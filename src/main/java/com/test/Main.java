d538b2b5efe7be9be24d902bc9fa38a4db0c12c3
kamontat <kamontat_c@hotmail.com>
2017-03-13 23:36:59 +0700

    add maven xml and main class

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import com.kamontat.convertion.Conversion;
+
+import java.io.IOException;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		String markdown = "I *don't* know, Who **are** you? But `I` Can't see.";
+		String html = "<p>I <em>don't</em> know, Who <strong>are</strong> you? But <code>I</code> Can't see.</p>";
+		
+		String h = Conversion.by(Conversion.Type.MD2HTML).convertString(markdown).toString();
+		System.out.println(h);
+		
+		String md = Conversion.by(Conversion.Type.HTML2MD).convertString(html).toString();
+		
+		System.out.println(html);
+		System.out.println(h);
+		
+		System.out.println(markdown);
+		System.out.println(md);
+	}
+}

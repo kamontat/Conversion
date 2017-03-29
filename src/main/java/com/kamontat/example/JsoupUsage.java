5dc88aa32afd59f93f40826794740bf36ea23979
kamontat <kamontat_c@hotmail.com>
2017-03-29 20:06:45 +0700

    add jsoup util with html string

--- src/main/java/com/kamontat/example/JsoupUsage.java
+++ src/main/java/com/kamontat/example/JsoupUsage.java
@@ -2,6 +2,7 @@ package com.kamontat.example;
 
 import com.kamontat.utilities.HtmlUtil;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * @author kamontat
@@ -11,19 +12,65 @@ import org.jsoup.nodes.Element;
 public class JsoupUsage {
 	public static void main(String[] args) {
 		String html = "<p>Error <code>Exception...</code> will appear when you <strong>don'Tag</strong> install <em>JRE</em></p>";
-		Element e1 = HtmlUtil.toHtmlByHtml(html);
-		Element e2 = HtmlUtil.toHtmlByPlain(html);
-		Element e3 = HtmlUtil.coverTag(html, "html");
-		Element e4 = HtmlUtil.coverTag(e1, "body");
+		
+		// add html tag (include body and head too)
+		Element eFullHtml = HtmlUtil.toHtmlByHtml(html);
+		// same
+		// Element eFullHtml = HtmlUtil.coverTag(html, "html");
+		
+		// thing parameter as plain text (so it will convert input to text that can add to html)
+		Element eFullPlain = HtmlUtil.toHtmlByPlain(html);
+		
+		// add multiple tag
+		Element eBody = HtmlUtil.coverTag(html, "body", "div");
+		
+		Elements eCode = HtmlUtil.getHtmlInTag(html, "code");
+		String sCode = HtmlUtil.removeTag(eCode);
+		
+		// output
+		System.out.println("-------------------------------");
+		System.out.println(eFullHtml);
+		/*
+		 <html>
+		  <head></head>
+		  <body>
+		   <p>Error <code>Exception...</code> will appear when you <strong>don'Tag</strong> install <em>JRE</em></p>
+		  </body>
+		 </html>
+		 */
 		
 		System.out.println("-------------------------------");
-		System.out.println(e1);
+		System.out.println(eFullPlain);
+		/*
+		  <html>
+		   &lt;p&gt;Error &lt;code&gt;Exception...&lt;/code&gt; will appear when you &lt;strong&gt;don'Tag&lt;/strong&gt; install &lt;em&gt;JRE&lt;/em&gt;&lt;/p&gt;
+		  </html>
+		 */
+		
 		System.out.println("-------------------------------");
-		System.out.println(e2);
+		System.out.println(eBody);
+		/*
+		  <body>
+		   <div>
+		    <p>Error <code>Exception...</code> will appear when you <strong>don'Tag</strong> install <em>JRE</em></p>
+		   </div>
+		  </body>
+		 */
+		
 		System.out.println("-------------------------------");
-		System.out.println(e3);
+		System.out.println(eCode);
+		/*
+		  <code>Exception...</code>
+		 */
+		
 		System.out.println("-------------------------------");
-		System.out.println(e4);
+		System.out.println(sCode);
+		/*
+		  Exception...
+		 */
+		
 		System.out.println("-------------------------------");
+		
+		// more than is, you must learn by yourself
 	}
 }
--- src/main/java/com/kamontat/utilities/HtmlUtil.java
+++ src/main/java/com/kamontat/utilities/HtmlUtil.java
@@ -1,6 +1,10 @@
 package com.kamontat.utilities;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.*;
 
 /**
  * html conversion by {@link org.jsoup.Jsoup} library
@@ -10,19 +14,46 @@ import org.jsoup.nodes.Element;
  * @since Mon 27/Mar/2017 - 11:49 PM
  */
 public class HtmlUtil {
+	public static Elements getHtmlInTag(String html, String tag) {
+		return Jsoup.parse(html).child(0).getElementsByTag(tag);
+	}
+	
+	public static String removeTag(String html) {
+		return Jsoup.parse(html).child(0).html();
+	}
+	
+	public static String removeTag(Elements html) {
+		return html.html();
+	}
+	
 	public static Element toHtmlByHtml(String html) {
-		return new Element("html").append(html);
+		return addTag(html, "html");
 	}
 	
 	public static Element toHtmlByPlain(String plainText) {
 		return new Element("html").text(plainText);
 	}
 	
-	public static Element coverTag(String html, String tagName) {
-		return new Element(tagName).append(html);
+	public static Element coverTag(String html, String... tagNames) {
+		List<String> tags = Arrays.asList(tagNames);
+		Collections.reverse(tags);
+		Element e = null;
+		for (String s : tags) {
+			if (e == null) e = addTag(html, s);
+			else e = addTag(e, s);
+		}
+		return e;
 	}
 	
-	public static Element coverTag(Element html, String tagName) {
-		return new Element(tagName).append(html.toString());
+	public static Element coverTag(Element html, String... tagName) {
+		return coverTag(html.toString(), tagName);
+	}
+	
+	private static Element addTag(String s, String tagName) {
+		return new Element(tagName).append(s);
+	}
+	
+	private static Element addTag(Element s, String tagName) {
+		return new Element(tagName).appendChild(s);
 	}
 }
